599fb3704c8635fc1cc3d78379e87823c04288e7
shubtiwari <shubham.tiwari@coloredcow.com>
2022-03-10 14:10:32 +0530

    fix: formatting

--- server/model-manuscript/src/graphql.js
+++ server/model-manuscript/src/graphql.js
@@ -435,8 +435,8 @@ const resolvers = {
         const review = {
           recommendation: '',
           isDecision: false,
-          isHiddenReviewerName:true,
-          isHiddenFromAuthor:true,
+          isHiddenReviewerName: true,
+          isHiddenFromAuthor: true,
           userId: context.user,
           manuscriptId: team.manuscriptId,
         }
