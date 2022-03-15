0e1e2d28e116e2502a00476a645efb7757c5b01a
abhishek-pokhriyal <pokhi@coloredcow.com>
2022-03-15 11:00:19 +0530

    refactor: moved render logic from DashboardPage to Dashboard

--- app/components/component-dashboard/src/components/DashboardPage.js
+++ app/components/component-dashboard/src/components/DashboardPage.js
@@ -1,26 +1,13 @@
 import React from 'react'
 import { useQuery, useMutation } from '@apollo/client'
-import { Button } from '@pubsweet/ui'
 // import Authorize from 'pubsweet-client/src/helpers/Authorize'
 
 import config from 'config'
 import ReactRouterPropTypes from 'react-router-prop-types'
 import queries from '../graphql/queries'
 import mutations from '../graphql/mutations'
-import { Container, Placeholder } from '../style'
-import EditorItem from './sections/EditorItem'
-import OwnerItem from './sections/OwnerItem'
-import ReviewerItem from './sections/ReviewerItem'
-import {
-  Spinner,
-  SectionHeader,
-  Title,
-  SectionRow,
-  SectionContent,
-  Heading,
-  HeadingWithAction,
-  CommsErrorBanner,
-} from '../../../shared'
+import Dashboard from './Dashboard'
+import { Spinner, CommsErrorBanner } from '../../../shared'
 
 const getLatestVersion = manuscript => {
   if (
@@ -44,11 +31,6 @@ const getManuscriptsUserHasRoleIn = (manuscripts, userId, roles) =>
         t.members.some(member => member.user.id === userId),
     ),
   )
-
-const getRoles = (m, userId) =>
-  m.teams
-    .filter(t => t.members.some(member => member.user.id === userId))
-    .map(t => t.role)
 
 const DashboardPage = ({ history, ...props }) => {
   const { loading, data, error } = useQuery(queries.dashboard, {
@@ -101,88 +83,18 @@ const DashboardPage = ({ history, ...props }) => {
   // Editors are always linked to the parent/original manuscript, not to versions
 
   const urlFrag = config.journal.metadata.toplevel_urlfragment
+  const instanceName = process.env.INSTANCE_NAME
 
   return (
-    <Container>
-      <HeadingWithAction>
-        <Heading>Dashboard</Heading>
-        <Button
-          onClick={() => history.push(`${urlFrag}/newSubmission`)}
-          primary
-        >
-          ＋ New submission
-        </Button>
-      </HeadingWithAction>
-      {!['ncrc'].includes(process.env.INSTANCE_NAME) && (
-        <SectionContent>
-          <SectionHeader>
-            <Title>My Submissions</Title>
-          </SectionHeader>
-          {authorLatestVersions.length > 0 ? (
-            authorLatestVersions.map(version => (
-              // Links are based on the original/parent manuscript version
-              <OwnerItem
-                key={version.id}
-                // deleteManuscript={() =>
-                //   // eslint-disable-next-line no-alert
-                //   window.confirm(
-                //     'Are you sure you want to delete this submission?',
-                //   ) && deleteManuscript({ variables: { id: submission.id } })
-                // }
-                version={version}
-              />
-            ))
-          ) : (
-            <Placeholder>
-              You have not submitted any manuscripts yet
-            </Placeholder>
-          )}
-        </SectionContent>
-      )}
-      {!['ncrc'].includes(process.env.INSTANCE_NAME) && (
-        <SectionContent>
-          <SectionHeader>
-            <Title>To Review</Title>
-          </SectionHeader>
-          {reviewerLatestVersions.length > 0 ? (
-            reviewerLatestVersions.map(version => (
-              <ReviewerItem
-                currentUser={currentUser}
-                key={version.id}
-                reviewerRespond={reviewerRespond}
-                version={version}
-              />
-            ))
-          ) : (
-            <Placeholder>
-              You have not been assigned any reviews yet
-            </Placeholder>
-          )}
-        </SectionContent>
-      )}
-
-      <SectionContent>
-        <SectionHeader>
-          <Title>Manuscripts I&apos;m editor of</Title>
-        </SectionHeader>
-        {editorLatestVersions.length > 0 ? (
-          editorLatestVersions.map(manuscript => (
-            <SectionRow key={`manuscript-${manuscript.id}`}>
-              <EditorItem
-                currentRoles={getRoles(manuscript, currentUser.id)}
-                version={manuscript}
-              />
-            </SectionRow>
-          ))
-        ) : (
-          <SectionRow>
-            <Placeholder>
-              You are not an editor of any manuscript yet
-            </Placeholder>
-          </SectionRow>
-        )}
-      </SectionContent>
-    </Container>
+    <Dashboard
+      authorLatestVersions={authorLatestVersions}
+      currentUser={currentUser}
+      editorLatestVersions={editorLatestVersions}
+      instanceName={instanceName}
+      newSubmission={() => history.push(`${urlFrag}/newSubmission`)}
+      reviewerLatestVersions={reviewerLatestVersions}
+      reviewerRespond={reviewerRespond}
+    />
   )
 }
 
