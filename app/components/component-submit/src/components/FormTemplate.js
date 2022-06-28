62874d3e527ea052ef621c79ba15f36f160e846e
Ben Whitmore <ben.whitmore0@gmail.com>
2022-06-28 22:40:04 +1200

    fix(threaded-discussion): fix console error when submitting

--- app/components/component-submit/src/components/FormTemplate.js
+++ app/components/component-submit/src/components/FormTemplate.js
@@ -423,13 +423,13 @@ const FormTemplate = ({
     await Promise.all(
       form.children
         .filter(field => field.component === 'ThreadedDiscussion')
-        .forEach(field => {
-          const threadedDiscussionId = get(values, field.name)
-          if (threadedDiscussionId)
-            completeComments({
-              variables: { threadedDiscussionId, userId: currentUser.id },
-            })
-        }),
+        .map(field => get(values, field.name))
+        .filter(Boolean)
+        .map(async threadedDiscussionId =>
+          completeComments({
+            variables: { threadedDiscussionId, userId: currentUser.id },
+          }),
+        ),
     )
   }
 
