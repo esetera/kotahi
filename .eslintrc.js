d5aa3dcb134788772b94f46a95bd1374136e437b
deepankar-sajwan <deepankar.sajwan@coloredcow.in>
2022-08-18 17:54:34 +0530

    fix(issue-865): updated .eslintrc.js with rule to avoid class method

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,6 @@
 const { eslint } = require('@coko/lint')
 
+// Added parserOptions to remove the @decorators issues
 eslint.parserOptions = {
   ecmaVersion: 6,
   ecmaFeatures: {
@@ -7,6 +8,11 @@ eslint.parserOptions = {
     experimentalObjectRestSpread: true,
   },
 }
+// Added rules to avoid class method
+eslint.rules['class-methods-use-this'] = [
+  1,
+  { exceptMethods: ['run', 'enable', 'active', 'select'] },
+]
 
 eslint.rules['react/jsx-props-no-spreading'] = 0
 
