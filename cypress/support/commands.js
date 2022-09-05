e611b312c579ea0b99f415db7b804aecb202a8d1
deepankar-sajwan <deepankar.sajwan@coloredcow.in>
2022-09-05 12:19:30 +0530

    fix(issue-865): updating code in  command.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -40,9 +40,10 @@ Cypress.Commands.add('login', (name, page) => {
   //   cy.setToken(token)
   //   cy.visit(page)
   cy.task('createToken', name).then(token => {
-    const userToken = token
+    // const userToken = token
     // cy.setToken(userToken)
-    localStorage.setItem('token', userToken)
+    cy.wrap(token).as('token')
+    localStorage.setItem('token', token)
     cy.visit(page)
   })
 })
