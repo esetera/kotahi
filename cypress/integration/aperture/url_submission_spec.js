2e4bc3badb30645d5e3997e0a80d67c91389cdb4
deepankar-sajwan <deepankar.sajwan@coloredcow.in>
2022-09-28 20:14:31 +0530

    fix(issue-865): updated code for url submission case

--- cypress/integration/aperture/url_submission_spec.js
+++ cypress/integration/aperture/url_submission_spec.js
@@ -18,7 +18,7 @@ describe('Upload manuscript test', () => {
     })
 
     // Click on new submission
-    DashboardPage.clickSubmissionButton() 
+    cy.get('button').contains('＋ New submission').click()
 
     // Upload manuscript
     cy.get('button').contains('Submit a URL instead').click()
