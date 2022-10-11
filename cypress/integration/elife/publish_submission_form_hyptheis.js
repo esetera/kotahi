0eaf4e8a348145f4e7292eddfc91ab40ff69300b
deepankar-sajwan <deepankar.sajwan@coloredcow.in>
2022-10-11 17:18:07 +0530

    added test case for publishing submission form to hypothesis group

--- cypress/integration/elife/publish_submission_form_hyptheis.js
+++ cypress/integration/elife/publish_submission_form_hyptheis.js
@@ -25,21 +25,21 @@ describe('Form builder', () => {
       'contain',
       'Research Object Submission Form',
     )
-    FormsPage.clickFormOption(1)
+    FormsPage.clickFormOption(3)
     FormsPage.getFieldValidate()
-    cy.get(':nth-child(8) > .style__Legend-sc-1npdrat-1')
-    cy.get(
-      ':nth-child(8) > :nth-child(2) > .css-3x5r4n-container > .react-select__control > .react-select__value-container',
-    ).click()
-    cy.get('.react-select__option').eq(0).click()
-    cy.contains('Update Field').click()
-    // adding a field in submission form
-    cy.contains('Add Field').click({ force: true })
-    cy.contains('Choose in the list').click()
-    cy.get('button')
-    cy.contains('VisualAbstract').click()
-    cy.contains('Name (internal field name)').click()
-    cy.get('[name=name]').type('submission.visualAbstract')
-    cy.contains('Update Field').click()
+    // cy.get(':nth-child(8) > .style__Legend-sc-1npdrat-1')
+    // cy.get(
+    //   ':nth-child(8) > :nth-child(2) > .css-3x5r4n-container > .react-select__control > .react-select__value-container',
+    // ).click()
+    // cy.get('.react-select__option').eq(0).click()
+    // cy.contains('Update Field').click()
+    // // adding a field in submission form
+    // cy.contains('Add Field').click({ force: true })
+    // cy.contains('Choose in the list').click()
+    // cy.get('button')
+    // cy.contains('VisualAbstract').click()
+    // cy.contains('Name (internal field name)').click()
+    // cy.get('[name=name]').type('submission.visualAbstract')
+    // cy.contains('Update Field').click()
     })
 })
