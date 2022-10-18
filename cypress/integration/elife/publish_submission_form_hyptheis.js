383077c2e6eb65b458ea66dccf58b5f89281c13e
deepankar-sajwan <deepankar.sajwan@coloredcow.in>
2022-10-18 13:08:37 +0530

    fix(issue-841): updated code for publishing to publishing submission

--- cypress/integration/elife/publish_submission_form_hyptheis.js
+++ cypress/integration/elife/publish_submission_form_hyptheis.js
@@ -6,7 +6,8 @@ import { ManuscriptsPage } from '../../page-object/manuscripts-page'
 import { SubmissionFormPage } from '../../page-object/submission-form-page'
 import { dashboard } from '../../support/routes'
 
-const bioRxivArticleUrl = 'https://www.biorxiv.org/content/10.1101/2022.05.28.493855v1'
+const bioRxivArticleUrl =
+  'https://www.biorxiv.org/content/10.1101/2022.05.28.493855v1'
 
 describe('Update the submission form field', () => {
   it('update submission form field for publishing to hypothesis group', () => {
@@ -26,20 +27,18 @@ describe('Update the submission form field', () => {
     cy.contains('Submission').click()
 
     // For Submission field
-    FormsPage.getFormTitleTab(0).should(
-      'contain',
-      'eLife Submission Form',
-    )
+    FormsPage.getFormTitleTab(0).should('contain', 'eLife Submission Form')
     FormsPage.clickFormOption(3)
     FormsPage.getFieldValidate()
 
     cy.get(':nth-child(3) > .sc-dmlrTW').contains('Always').click()
-    cy.get(':nth-child(15) > :nth-child(2)').type('test_tag')
+    cy.get('form > :nth-child(15)').type('test_tag')
     cy.contains('Update Field').click({ force: true })
     Menu.clickManuscripts()
-    DashboardPage.clickSubmissionButton() 
+    DashboardPage.clickSubmissionButton()
     // Upload manuscript
-    cy.get('button').contains('Submit a URL instead').click()   
+    cy.get('button').contains('Submit a URL instead').click()
+    // eslint-disable-next-line jest/valid-expect-in-promise
     cy.fixture('submission_form_data').then(data => {
       SubmissionFormPage.fillInArticleld(data.articleId)
       SubmissionFormPage.fillInDoi(data.doi)
@@ -54,15 +53,12 @@ describe('Update the submission form field', () => {
       cy.awaitDisappearSpinner()
       cy.intercept('/graphql').as('getResponse')
       ManuscriptsPage.getOptionWithText('Publish').click()
-      cy.wait('@getResponse')
-        .its('response')
-        .should('deep.include',{
-          statusCode: 200,
-          statusMessage: "OK"
-        })
+      cy.wait('@getResponse').its('response').should('deep.include', {
+        statusCode: 200,
+        statusMessage: 'OK',
+      })
       SubmissionFormPage.waitThreeSec()
       ManuscriptsPage.getStatusField(0).contains('Published')
     })
   })
 })
-   
