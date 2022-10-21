0d31ce04f0ed8ad60e56a0937fd36efad2adec7f
deepankar-sajwan <deepankar.sajwan@coloredcow.in>
2022-10-21 15:53:13 +0530

    fix(issue-841): added test case for form builder functionality

--- cypress/integration/colab/new_manuscript_version_spec.js
+++ cypress/integration/colab/new_manuscript_version_spec.js
@@ -4,51 +4,52 @@ import { ControlPage } from '../../page-object/control-page'
 import { ManuscriptsPage } from '../../page-object/manuscripts-page'
 import { SubmissionFormPage } from '../../page-object/submission-form-page'
 import { dashboard } from '../../support/routes'
+
 const decisionTextContent = 'Please fix Foo in the Paper!'
 const decisionFileName = 'test-pdf.pdf'
 
 describe('checking manuscript version', () => {
   it('editor checks for new manuscript version', () => {
     cy.task('restore', 'commons/colab_bootstrap')
-    cy.task('seed', 'three_reviews_completed') 
+    cy.task('seed', 'three_reviews_completed')
     cy.task('seedForms')
     // eslint-disable-next-line jest/valid-expect-in-promise
     cy.fixture('role_names').then(name => {
       cy.login(name.role.admin.name, dashboard)
       DashboardPage.clickManuscriptNavButton()
-      ManuscriptsPage.clickControlButton()  
+      ManuscriptsPage.clickControlButton()
       /* Assign Editor */
       ControlPage.getAssignSeniorEditorDropdown()
         .click({ force: true })
-        .type(`${name.role.seniorEditor.username}{enter}`)  
+        .type(`${name.role.seniorEditor.username}{enter}`)
 
-       /* Editor  Submits a decision */
-       cy.login(name.role.seniorEditor.name, dashboard)
-       DashboardPage.clickControlPanel()
-       /* Verify publish button is disabled */
-       ControlPage.getPublishButton().should('be.disabled')  
-       /* Fill the decision form */
-       ControlPage.clickDecisionTextInput()
-       ControlPage.getDecisionTextInput().type(decisionTextContent)
-       ControlPage.getDecisionFileInput().attachFile(decisionFileName)
-       ControlPage.clickRevise()
-       /* Submit the decision */
-       ControlPage.clickSubmitDecisionButton()  
-       /* Check appears in front of button */
-       ControlPage.checkSvgExists()  
+      /* Editor  Submits a decision */
+      cy.login(name.role.seniorEditor.name, dashboard)
+      DashboardPage.clickControlPanel()
+      /* Verify publish button is disabled */
+      ControlPage.getPublishButton().should('be.disabled')
+      /* Fill the decision form */
+      ControlPage.clickDecisionTextInput()
+      ControlPage.getDecisionTextInput().type(decisionTextContent)
+      ControlPage.getDecisionFileInput().attachFile(decisionFileName)
+      ControlPage.clickRevise()
+      /* Submit the decision */
+      ControlPage.clickSubmitDecisionButton()
+      /* Check appears in front of button */
+      ControlPage.checkSvgExists()
 
       /* View Decision as an Author */
-      cy.login(name.role.author.name, dashboard) 
+      cy.login(name.role.author.name, dashboard)
       /* Click on first MySubmission */
-      DashboardPage.getSubmittedManuscript().click()  
+      DashboardPage.getSubmittedManuscript().click()
       /* Verify Decision Content */
       DashboardPage.getDecisionField(0).should('contain', decisionTextContent)
       DashboardPage.getDecisionField(1).should('contain', decisionFileName)
       DashboardPage.getDecisionField(2).should('contain', 'revise')
       /* Create new manuscript version */
-      DashboardPage.clickCreateNewVersionButton() 
+      DashboardPage.clickCreateNewVersionButton()
       SubmissionFormPage.waitThreeSec()
-       // eslint-disable-next-line jest/valid-expect-in-promise
+      // eslint-disable-next-line jest/valid-expect-in-promise
       cy.fixture('submission_form_data').then(data => {
         SubmissionFormPage.fillInAbstractColab(data.abstract)
         SubmissionFormPage.getWaxInputBox(0).fillInput(data.abstract)
@@ -67,13 +68,13 @@ describe('checking manuscript version', () => {
       /* Verify new submission got created */
       DashboardPage.getSubmittedManuscript()
         .contains('test pdf')
-        .should('exist') 
+        .should('exist')
 
       /* Login as editor and check the new version submission form */
       cy.login(name.role.seniorEditor.name, dashboard)
       DashboardPage.clickControlPanel()
       /* Verify publish button is disabled */
-      ControlPage.getPublishButton().should('be.disabled') 
+      ControlPage.getPublishButton().should('be.disabled')
     })
   })
 })
