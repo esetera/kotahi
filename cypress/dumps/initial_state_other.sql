a35b42a9610f1018e269df65cb00375a9fe40289
Ben Whitmore <ben.whitmore0@gmail.com>
2022-11-15 17:01:55 +1300

    test: restore more sql to isolate error

--- cypress/dumps/initial_state_other.sql
+++ cypress/dumps/initial_state_other.sql
@@ -1080,10 +1080,6 @@ CREATE TABLE public.invitations (
 );
 
 
-ALTER TABLE public.reviews ADD CONSTRAINT reviews_pkey PRIMARY KEY (id);
-ALTER TABLE public.reviews ADD CONSTRAINT reviews_manuscript_id_fkey FOREIGN KEY (manuscript_id)
-  REFERENCES public.manuscripts(id) ON DELETE CASCADE;
-
 
 CREATE TABLE public.email_blacklist (
   id UUID PRIMARY KEY,
@@ -1106,3 +1102,122 @@ ALTER TABLE public.threaded_discussions
   ADD CONSTRAINT threaded_discussions_manuscript_id_fkey FOREIGN KEY (manuscript_id) REFERENCES public.manuscripts(id) ON DELETE CASCADE;
 
 
+
+
+
+CREATE FUNCTION public.manuscripts_searchable_text_trigger() RETURNS trigger AS $$
+DECLARE
+  a TEXT;
+  b TEXT;
+  c TEXT;
+  authors JSON;
+  author JSON;
+  authorstext TEXT := '';
+  member RECORD;
+  memberstext TEXT := '';
+  invitee RECORD;
+  inviteestext TEXT := '';
+  submittertext TEXT := '';
+BEGIN
+  authors := COALESCE(NULLIF(new.submission->>'authors',''), NULLIF(new.submission->>'authorNames',''), '[]')::JSON;
+  FOR author IN SELECT * FROM json_array_elements(authors) LOOP
+    authorstext = concat(
+      authorstext,
+      concat_ws(', ',
+        concat_ws(' ', author->>'firstName', author->>'lastName'),
+        author->>'email',
+        author->>'affiliation'
+      ),
+      '; '
+    );
+  END LOOP;
+
+  FOR member IN SELECT username, email
+    FROM public.teams t, public.team_members tm, public.users u
+    WHERE t.object_id = new.id AND tm.team_id = t.id AND u.id = tm.user_id
+  LOOP
+    memberstext := concat(
+      memberstext,
+      member.username, ' ', member.email,
+      '; '
+    );
+  END LOOP;
+
+  FOR invitee IN SELECT invited_person_name, to_email
+    FROM public.invitations i
+    WHERE i.manuscript_id = new.id
+  LOOP
+    inviteestext := concat(
+      inviteestext,
+      invitee.invited_person_name, ' ', invitee.to_email,
+      '; '
+    );
+  END LOOP;
+
+  submittertext := (SELECT concat_ws(', ', username, email)
+    FROM public.users u
+    WHERE u.id = new.submitter_id);
+
+  a := concat_ws(E'\n',
+    new.meta->>'title',
+    new.submission->>'description',
+    new.submission->>'title',
+    new.meta->>'abstract',
+    new.submission->>'abstract',
+    new.submission->>'doi',
+    new.submission->>'DOI',
+    new.submission->>'articleURL'
+  );
+  b := concat_ws(E'\n',
+    authorstext,
+    memberstext,
+    inviteestext,
+    submittertext,
+    new.short_id,
+    new.submission->>'articleId',
+    new.submission->>'link',
+    new.submission->>'biorxivURL'
+  );
+  c := concat(
+    new.meta->>'source'
+  );
+
+  new.search_tsvector :=
+    setweight(to_tsvector('english', a), 'A') ||
+    setweight(to_tsvector('english', b), 'B') ||
+    setweight(to_tsvector('english', c), 'C');
+  new.searchable_text := concat_ws(E'\n', a, b, c);
+
+  RETURN new;
+END
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER searchable_text_update BEFORE INSERT OR UPDATE
+  ON public.manuscripts FOR EACH ROW EXECUTE PROCEDURE public.manuscripts_searchable_text_trigger();
+
+CREATE FUNCTION public.team_members_trigger() RETURNS trigger AS $$
+BEGIN
+  UPDATE public.manuscripts m SET updated = m.updated -- Cause trigger function to regenerate searchable_text
+    FROM public.teams t
+    WHERE new.team_id = t.id AND t.object_id = m.id;
+  RETURN new;
+END
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER team_member_update AFTER INSERT OR UPDATE
+  ON public.team_members FOR EACH ROW EXECUTE PROCEDURE public.team_members_trigger();
+
+CREATE FUNCTION public.invitations_trigger() RETURNS trigger AS $$
+BEGIN
+  UPDATE public.manuscripts m SET updated = m.updated -- Cause trigger function to regenerate searchable_text
+    WHERE new.manuscript_id = m.id;
+  RETURN new;
+END
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER invitation_update AFTER INSERT OR UPDATE
+  ON public.invitations FOR EACH ROW EXECUTE PROCEDURE public.invitations_trigger();
+
+
+CREATE INDEX IF NOT EXISTS manuscripts_search_idx ON public.manuscripts
+  USING GIN (search_tsvector);
