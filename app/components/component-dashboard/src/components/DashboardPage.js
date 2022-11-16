6503955bf66f19cd2819e5e958912320039780d7
ericcccliu <liueric360@gmail.com>
2022-11-15 21:48:39 -0600

    chore: fix rebase issues in DashboardPage

--- app/components/component-dashboard/src/components/DashboardPage.js
+++ app/components/component-dashboard/src/components/DashboardPage.js
@@ -6,6 +6,9 @@ import config from 'config'
 import { Redirect } from 'react-router-dom'
 import ReactRouterPropTypes from 'react-router-prop-types'
 import Dashboard from './Dashboard'
+import mutations from '../graphql/mutations'
+import { UPDATE_MEMBER_STATUS_MUTATION } from '../../../../queries/team'
+
 
 const DashboardPage = ({ history, ...props }) => {
   const invitationId = window.localStorage.getItem('invitationId')
@@ -15,66 +18,6 @@ const DashboardPage = ({ history, ...props }) => {
   if (invitationId) {
     return <Redirect to="/invitation/accepted" />
   }
-
-  const [reviewerRespond] = useMutation(mutations.reviewerResponseMutation)
-
-  const [updateMemberStatus] = useMutation(UPDATE_MEMBER_STATUS_MUTATION)
-
-  const [removeTaskAlertsForCurrentUser] = useMutation(
-    mutations.removeTaskAlertsForCurrentUserMutation,
-  )
-
-  // const [deleteManuscript] = useMutation(mutations.deleteManuscriptMutation, {
-  //   update: (cache, { data: { deleteManuscript } }) => {
-  //     const data = cache.readQuery({ query: queries.dashboard })
-  //     const manuscripts = data.manuscripts.filter(
-  //       manuscript => manuscript.id !== deleteManuscript,
-  //     )
-  //     cache.writeQuery({
-  //       query: queries.dashboard,
-  //       data: {
-  //         manuscripts,
-  //       },
-  //     })
-  //   },
-  // })
-
-  // eslint-disable-next-line no-unused-vars
-  const [createNewTaskAlerts] = useMutation(
-    mutations.createNewTaskAlertsMutation,
-  )
-
-  // Dismiss any alerts only after the page is fully loaded, so the alert indicator remains visible
-  useEffect(() => {
-    const removeAlertsFunc = async () => removeTaskAlertsForCurrentUser()
-    if (data) removeAlertsFunc()
-  }, [!!data])
-
-  if (loading) return <Spinner />
-  if (error) return <CommsErrorBanner error={error} />
-  const currentUser = data && data.currentUser
-
-  const latestVersions = data.manuscriptsUserHasCurrentRoleIn.map(
-    getLatestVersion,
-  )
-
-  const authorLatestVersions = getManuscriptsUserHasRoleIn(
-    latestVersions,
-    currentUser.id,
-    ['author'],
-  )
-
-  const reviewerLatestVersions = getManuscriptsUserHasRoleIn(
-    latestVersions,
-    currentUser.id,
-    ['reviewer', 'invited:reviewer', 'accepted:reviewer', 'completed:reviewer'],
-  )
-
-  const editorLatestVersions = getManuscriptsUserHasRoleIn(
-    latestVersions,
-    currentUser.id,
-    ['seniorEditor', 'handlingEditor', 'editor'],
-  )
 
   // Editors are always linked to the parent/original manuscript, not to versions
 
@@ -88,10 +31,6 @@ const DashboardPage = ({ history, ...props }) => {
 
   return (
     <Dashboard
-      authorLatestVersions={authorLatestVersions}
-      createNewTaskAlerts={null /* For testing only: createNewTaskAlerts */}
-      currentUser={currentUser}
-      editorLatestVersions={editorLatestVersions}
       instanceName={instanceName}
       newSubmission={() => history.push(`${urlFrag}/newSubmission`)}
       shouldShowShortId={shouldShowShortId}
