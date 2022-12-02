0a3fb997112ce5e479d0a66ef6bca1080074d088
Pete Stenger <pete@stenger.io>
2022-12-02 03:54:43 +0000

    Add Review status badge

--- config/journal/review-status.js
+++ config/journal/review-status.js
@@ -3,25 +3,30 @@ module.exports = [
     color: '#fff2cd',
     label: 'Invited',
     value: 'invited',
+    lightText: false,
   },
   {
     color: '#d7efd4',
     label: 'Accepted',
     value: 'accepted',
+    lightText: false,
   },
   {
     color: '#ffcc37',
     label: 'In Progress',
     value: 'inProgress',
+    lightText: false,
   },
   {
     color: '#56b984',
     label: 'Completed',
     value: 'completed',
+    lightText: true,
   },
   {
     color: '#c23d20',
     label: 'Declined',
     value: 'rejected',
+    lightText: true,
   },
 ]
