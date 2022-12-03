885735535dadc592744952c16286bdba25392b96
Pete Stenger <pete@stenger.io>
2022-12-03 16:23:26 -0600

    chore: remove comment

--- app/components/component-manuscripts-table/src/cell-components/LastUpdated.js
+++ app/components/component-manuscripts-table/src/cell-components/LastUpdated.js
@@ -2,11 +2,6 @@ import React from 'react'
 import styled from 'styled-components'
 import { getMembersOfTeam } from '../../../../shared/manuscriptUtils'
 import { convertTimestampToRelativeDateString } from '../../../../shared/dateUtils'
-// https://gitlab.coko.foundation/kotahi/kotahi/-/blob/peer-review-dashboard/app/components/component-review/src/components/reviewers/KanbanCard.js
-// convertTimestampToRelativeDateString(reviewer.updated)
-// https://gitlab.coko.foundation/kotahi/kotahi/-/blob/sort-kanban-cards/app/components/component-review/src/components/KanbanBoard.js
-//   const reviewersTeam = teams.find(team => team.role === 'reviewer') || {}
-// const reviewers = reviewersTeam.members || []
 
 const DateDisplay = styled.div`
   font-size: 14px;
