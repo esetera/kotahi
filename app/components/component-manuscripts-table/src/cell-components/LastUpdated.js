9859aff66a1cc32c5017e15d81f33b82667cb6c7
Pete Stenger <pete@stenger.io>
2022-12-03 19:52:50 -0600

    feat: display N/A if no data

--- app/components/component-manuscripts-table/src/cell-components/LastUpdated.js
+++ app/components/component-manuscripts-table/src/cell-components/LastUpdated.js
@@ -9,13 +9,14 @@ const DateDisplay = styled.div`
 `
 
 const LastUpdated = ({ manuscript }) => {
+  let updatedTimes = getMembersOfTeam(manuscript, 'reviewer').map(reviewer => reviewer.updated)
+  let timestamp = 'N/A'
+  if (updatedTimes.length > 0) {
+    timestamp = convertTimestampToRelativeDateString(updatedTimes.reduce((min, b) => (new Date(b) < new Date(min) ? b : min)))
+  }
   return (
     <DateDisplay>
-      {convertTimestampToRelativeDateString(
-        getMembersOfTeam(manuscript, 'reviewer')
-          .map(reviewer => reviewer.updated)
-          .reduce((min, b) => (new Date(b) < new Date(min) ? b : min)),
-      )}
+      {timestamp}
     </DateDisplay>
   )
 }
