37c24d3149486d4899c42e640933388b1d33837b
AndrewLester <alester220@gmail.com>
2023-01-13 22:51:43 -0500

    refactor: remove unnecessary fragment

--- app/components/component-manuscripts-table/src/cell-components/SubmitChevron.js
+++ app/components/component-manuscripts-table/src/cell-components/SubmitChevron.js
@@ -5,17 +5,13 @@ import { Icon } from '../../../shared'
 
 const SubmitChevron = ({ manuscript, urlFrag }) => {
   return (
-    <>
-      <Link
-        to={`${urlFrag}/versions/${
-          manuscript.parentId || manuscript.id
-        }/submit`}
-      >
-        <Icon color={theme.colorSecondary} noPadding size={2.5}>
-          chevron_right
-        </Icon>
-      </Link>
-    </>
+    <Link
+      to={`${urlFrag}/versions/${manuscript.parentId || manuscript.id}/submit`}
+    >
+      <Icon color={theme.colorSecondary} noPadding size={2.5}>
+        chevron_right
+      </Icon>
+    </Link>
   )
 }
 
