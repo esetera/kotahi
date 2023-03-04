7f499f53703b8bb37fadb53aa92a0a27681da63a
Ben Whitmore <ben.whitmore0@gmail.com>
2023-03-05 01:11:05 +1300

    fix(invite): get id from mutation, to allow auto cache update

--- app/queries/invitation/index.js
+++ app/queries/invitation/index.js
@@ -7,6 +7,7 @@ export const UPDATE_INVITATION_RESPONSE = gql`
       responseComment: $responseComment
       declinedReason: $declinedReason
     ) {
+      id
       responseComment
       declinedReason
       toEmail
