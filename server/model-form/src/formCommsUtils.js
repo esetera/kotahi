8a7185a0de87b6170f43d1d9049bd81ee8dd869b
Ben Whitmore <ben.whitmore0@gmail.com>
2023-05-26 15:37:55 +1200

    feat(form-builder): create routine to migrate fields

--- server/model-form/src/formCommsUtils.js
+++ server/model-form/src/formCommsUtils.js
@@ -1,4 +1,5 @@
 const models = require('@pubsweet/models')
+const { ref } = require('objection')
 
 const getActiveForms = async () => {
   const forms = await models.Form.query()
@@ -34,4 +35,67 @@ const getPublicFields = async (purpose, category) => {
     }))
 }
 
-module.exports = { getPublicFields, getActiveForms }
+/** Change field name in form, and in all form-data for all manuscripts.
+ * Avoid creating duplicate field names (though this is not guaranteed for
+ * old manuscripts that contain different fields to the current form).
+ */
+const migrateFieldName = async (formId, oldFieldName, newFieldName) => {
+  const form = await models.Form.query().findById(formId)
+  if (!form)
+    throw new Error(
+      `Cannot change field name in form ${formId}: the form was not found`,
+    )
+
+  const field = form.structure.children.some(f => f.name === oldFieldName)
+  if (!field)
+    throw new Error(
+      `Cannot change field name in form ${formId}: field '${oldFieldName}' was not found`,
+    )
+
+  if (oldFieldName === newFieldName) return // Nothing to do
+
+  if (form.structure.children.some(f => f.name === newFieldName))
+    throw new Error(
+      `Cannot change field name in form ${formId}: a field named '${newFieldName}' already exists`,
+    )
+
+  const nameValidationRegex =
+    form.purpose === 'submit'
+      ? /^(?:submission\.[a-zA-Z]\w*|meta.title|meta.abstract|fileName|visualAbstract|manuscriptFile)$/
+      : /^[a-zA-Z]\w*$/
+
+  if (
+    !nameValidationRegex.test(oldFieldName) ||
+    !nameValidationRegex.test(newFieldName)
+  )
+    throw new Error(
+      `Cannot change field name in form ${formId} from '${oldFieldName}' to '${newFieldName}': illegal name`,
+    )
+
+  if (form.purpose === 'submit') {
+    const pgOldField = oldFieldName.replace('.', ':')
+    const pgNewField = newFieldName.replace('.', ':')
+
+    await models.Manuscript.query().patch({
+      [pgNewField]: ref(pgOldField),
+      [pgOldField]: null,
+    })
+  } else if (form.purpose === 'review') {
+    await models.Review.query()
+      .patch({
+        [newFieldName]: ref(oldFieldName),
+        [oldFieldName]: null,
+      })
+      .whereNot({ isDecision: true })
+  } else {
+    // form.purpose === 'decision'
+    await models.Review.query()
+      .patch({
+        [newFieldName]: ref(oldFieldName),
+        [oldFieldName]: null,
+      })
+      .where({ isDecision: true })
+  }
+}
+
+module.exports = { getPublicFields, getActiveForms, migrateFieldName }
