42207703a49e2ce77dc65da8a1de82f23826e674
Vaibhav Rathore <vaibhav@coloredcow.com>
2023-07-18 13:23:31 +0530

    fix(1344): create or update global chat notification for a user

--- server/model-alert/src/graphql.js
+++ server/model-alert/src/graphql.js
@@ -1,4 +1,5 @@
 const Alert = require('./alert')
+const NotificationUserOption = require('./notificationUserOption')
 
 const resolvers = {
   Query: {
@@ -24,6 +25,42 @@ const resolvers = {
       }).save()
 
       return alert
+    },
+    updateGlobalChatNotificationOptionForUser: async (
+      _,
+      { userId, option },
+    ) => {
+      const globalChatOption = await NotificationUserOption.query()
+        .skipUndefined()
+        .where('user_id', userId)
+        .where('path', ['chat'])
+
+      // TODO: the code below can be replaced by a single upsert statement
+      if (!globalChatOption || globalChatOption.length === 0) {
+        const notificationOptionData = {
+          created: new Date(),
+          userId,
+          path: '{chat}',
+          option: 'off',
+        }
+
+        if (option === 'on') {
+          notificationOptionData.option = '30MinDigest'
+        }
+
+        await new NotificationUserOption(notificationOptionData).save()
+      } else {
+        let optionToSave = 'off'
+
+        if (option === 'on') {
+          optionToSave = '30MinDigest'
+        }
+
+        await NotificationUserOption.query()
+          .where('path', ['chat'])
+          .where('user_id', userId)
+          .patch({ option: optionToSave })
+      }
     },
   },
 }
