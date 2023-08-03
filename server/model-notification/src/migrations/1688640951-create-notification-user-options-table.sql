5a48fafa204e932eba9de3e23ead763d5bc62bf1
Vaibhav Rathore <vaibhav@coloredcow.com>
2023-08-03 22:17:05 +0530

    fix(1344): correction in migration

--- server/model-notification/src/migrations/1688640951-create-notification-user-options-table.sql
+++ server/model-notification/src/migrations/1688640951-create-notification-user-options-table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE notification_user_options (
   id UUID PRIMARY KEY,
-  created TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT current_timestamp,,
+  created TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT current_timestamp,
   updated TIMESTAMP WITH TIME ZONE,
   user_id UUID REFERENCES users(id) ON DELETE CASCADE,
   object_id UUID,
