b0c060af442d2dd8b4a551a1126dec8ad09dd1ff
Vaibhav Rathore <vaibhav@coloredcow.com>
2023-10-09 19:58:25 +0530

    fix(1282): pipeline without channel member unique constraint

--- server/model-channel/src/migrations/1695716032-add_unique_constraint_to_channel_members.sql
+++ server/model-channel/src/migrations/1695716032-add_unique_constraint_to_channel_members.sql
@@ -1,4 +1,4 @@
-DROP INDEX IF EXISTS channel_members_idx;
+-- DROP INDEX IF EXISTS channel_members_idx;
 
-ALTER TABLE channel_members
-ADD CONSTRAINT channel_members_idx UNIQUE (channel_id, user_id);
+-- ALTER TABLE channel_members
+-- ADD CONSTRAINT channel_members_idx UNIQUE (channel_id, user_id);
